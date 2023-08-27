c1f53517c4991ad7f2eca5dbb1908a9a0458c653
Vikash Pandey <pandeyvikash6991@gmail.com>
2023-08-27 20:06:24 +0530

    done

--- day-9-Assignment/index.js
+++ day-9-Assignment/index.js
@@ -1,13 +1,13 @@
-function  myFun(){
-    const color = document.getElementById("selectColor");
-    const btn =color.options[color.selectedIndex].value
-
-    console.log(btn);
-   
-}
-// function myFun(){
+// function  myFun(){
 //     const color = document.getElementById("selectColor");
-//     const btn = color.options[color.selectedIndex].value;
-//     document.body.style.backgroundColor=btn;
+//     const btn =color.options[color.selectedIndex].value
 
+//     console.log(btn);
+   
 // }
+function myFun(){
+    const color = document.getElementById("selectColor");
+    const btn = color.options[color.selectedIndex].value;
+    document.body.style.backgroundColor=btn;
+
+}
