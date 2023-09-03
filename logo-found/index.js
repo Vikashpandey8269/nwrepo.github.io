2fc0040483ce6f335619a245e7b17e97ceb51bbc
Vikash Pandey <pandeyvikash6991@gmail.com>
2023-09-03 22:23:03 +0530

    done

---
+++ logo-found/index.js
@@ -0,0 +1,30 @@
+// let url ="http://logo.clearbit.com/";
+// let companyUrl= "www.stackoverflow.com"
+
+// let logo = url+companyUrl
+
+// console.log(logo);
+
+// const logoC =document.getElementById("logoC");
+
+// const logoI =document.createElement("img");
+// logoI.src="logo"
+https://tallemucrm.com/
+
+//let logoF ="https://logo.clearbit.com/"
+let companyUrl ={"https://www.smarttouchinteractive.com/","https://tallemucrm.com/"}
+console.log(companyUrl);
+let url= logoF+companyUrl;
+
+const imageP =document.querySelector("#logoC");
+let ele=[]
+
+
+// function myFun(){
+//     let row = document.createElement("div");
+    
+   
+// row.innerHTML= ` <img src="${url}"/>`
+   
+//     imageP.appendChild(row)
+// }
